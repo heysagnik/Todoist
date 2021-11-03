228f6ae353c7d68cd69300258b1fa23b29b0f37b
Sagnik Sahoo <sahoosagnik1@gmail.com>
2021-11-03 21:46:30 +0530

    .

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,3 @@
 export const VIEWPORT_WIDTH: string = "600px";
-export const INPUT_PLACEHOLDER: string = "Recreate project using TypeScript...";
+export const INPUT_PLACEHOLDER: string = "What's up ? ...";
 export const TASKS_STORAGE_KEY: string = "tasksStorage";
